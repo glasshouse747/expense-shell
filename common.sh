c48b4a0e761bf67ef301e286ca0e7d0974b4f6e0
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 18:07:00 +0100

    Expense-bash script commit

--- common.sh
+++ common.sh
@@ -4,7 +4,10 @@ download_and_extract(){
 
   echo Download $component Code
   curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+  echo $?
 
   echo Extracting Frontend Code
   unzip /tmp/$component.zip &>>$log_file
+  echo $?
+
 }
