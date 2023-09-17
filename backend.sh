b4991af2b338670db82336a17015fa4326e71e57
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 15:22:57 +0100

    Expense-bash script commit

--- backend.sh
+++ backend.sh
@@ -1,38 +1,38 @@
 source common.sh
 
 echo Installation of NodeJS Repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 echo Copy Backend Service File
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo Install NodeJS
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo Add Application User
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo Clean App Files
-rm -rf /app >>$log_file
-mkdir /app >>$log_file
+rm -rf /app &>>$log_file
+mkdir /app &>>$log_file
 
 echo Download App Files
-curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
+curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
 
 echo Extract App Files
-unzip /tmp/backend.zip >>$log_file
+unzip /tmp/backend.zip &>>$log_file
 
 echo Download Dependencies for App
-npm install >>$log_file
+npm install &>>$log_file
 
 echo Start Backend Service Application
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo Install MySQL Client Application
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo Load Schema to MySQL
-mysql -h mysql.mydevops75.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h mysql.mydevops75.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
--- mysql.sh
+++ mysql.sh
@@ -1,17 +1,17 @@
 source common.sh
 
 echo Disable MySQL 8 Client Version
-dnf module disable mysql -y >>$log_file
+dnf module disable mysql -y &>>$log_file
 
 echo Copy MySQL Repo Files
-cp mysql.repo /etc/yum.repos.d/mysql.repo >>$log_file
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 
 echo Install MySQL Client Server
-dnf install mysql-community-server -y >>$log_file
+dnf install mysql-community-server -y &>>$log_file
 
 echo Start MySQL Client Server
-systemctl enable mysqld >>$log_file
-systemctl start mysqld >>$log_file
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
 
 echo Setup root Password for MySQL Client Server
-mysql_secure_installation --set-root-pass ExpenseApp@1 >>$log_file
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
