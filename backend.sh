a0f3ad9fa093268c153676784f83bedee2669f0f
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 17:53:56 +0100

    Expense-bash script commit

--- backend.sh
+++ backend.sh
@@ -2,37 +2,48 @@ source common.sh
 
 echo Installation of NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
+echo $?
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 echo Add Application User
 useradd expense &>>$log_file
+echo $?
 
 echo Clean App Files
 rm -rf /app &>>$log_file
 mkdir /app &>>$log_file
+echo $?
 
 echo Download App Files
 curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
+echo $?
 
 echo Extract App Files
 unzip /tmp/backend.zip &>>$log_file
+echo $?
 
 echo Download Dependencies for App
 npm install &>>$log_file
+echo $?
 
 echo Start Backend Service Application
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+echo $?
 
 echo Install MySQL Client Application
 dnf install mysql -y &>>$log_file
+echo $?
 
 echo Load Schema to MySQL
 mysql -h mysql.mydevops75.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
