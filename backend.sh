607a6f916474eb38882d01394393de6a416cad7e
Richard Okoro <glasshouse747@gmail.com>
2023-09-17 12:39:29 +0100

    Expense-bash script commit

--- backend.sh
+++ backend.sh
@@ -3,7 +3,9 @@ cp backend.service /etc/systemd/system/backend.service
 
 
 dnf install nodejs -y
+
 useradd expense
+rm -rf /app
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
