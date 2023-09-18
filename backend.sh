926b7691f0e0639009945758c69479af1df83545
Richard Okoro <glasshouse747@gmail.com>
2023-09-18 03:45:13 +0100

    Expense-bash script commit

--- backend.sh
+++ backend.sh
@@ -2,29 +2,53 @@ source common.sh
 
 echo Installation of NodeJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Add Application User
 useradd expense &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Clean App Files
 rm -rf /app &>>$log_file
 mkdir /app &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Download App Files
 curl -s -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Extract App Files
 unzip /tmp/backend.zip &>>$log_file
@@ -32,18 +56,34 @@ echo $?
 
 echo Download Dependencies for App
 npm install &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Start Backend Service Application
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Install MySQL Client Application
 dnf install mysql -y &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Load Schema to MySQL
 mysql -h mysql.mydevops75.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-echo $?
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
--- frontend.sh
+++ frontend.sh
@@ -3,17 +3,47 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Placing Expense Config File in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Removing Old Content from Nginx Html Folder
 rm -rf /usr/share/nginx/html/* &>>$log_file
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 cd /usr/share/nginx/html &>>$log_file
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 download_and_extract
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
 
 echo Starting Nginx Service
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
+if [$? -eq 0]; then
+  echo SUCCESS
+else
+  echo FAILED
+fi
